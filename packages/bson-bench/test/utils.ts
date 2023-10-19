393231bdb88d459ba5029473ff56680a81e25358
Warren James <warren.james@mongodb.com>
2023-10-19 11:24:24 -0400

    fix utils

--- packages/bson-bench/test/utils.ts
+++ packages/bson-bench/test/utils.ts
@@ -7,9 +7,9 @@ export { exists } from "../src/utils";
  * Remove all installed bson and bson-ext versions that have been installed by tests
  */
 export async function clearTestedDeps() {
-  for await (const dirent of await fs.opendir("node_modules")) {
+  for await (const dirent of await fs.opendir("../../node_modules")) {
     if (/^(bson-ext|bson)-(git|local)?.*$/.test(dirent.name)) {
-      fs.rm(path.join("node_modules", dirent.name), {
+      await fs.rm(path.join("../../node_modules", dirent.name), {
         recursive: true,
         force: true,
       });
